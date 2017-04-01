84e0695c0f2e821553ba6484d33f81cf665f0dfe
Jasmine Ellis <ellis.jas48@gmail.com>
2017-04-01 10:35:44 +0000

    Add content to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,48 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+### Ignore for now. Will seed database when geocoding is complete.
+
+genders = ["male", "female"]
+preferred_genders = genders.push("both")
+looking_fors = ["long-term dating", "short-term dating", "casual sex", "new friends"]
+religions = ["Agnosticism", "Atheism", "Christianity", "Judaism", "Catholicism", "Islam", "Buddhism", "Hinduism", "Sikh", "Other"]
+
+500.times do |n|
+  
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+  gender = genders.sample
+  date_of_birth = Faker::Date.between(Date.new(1949, 1, 1), Date.new(1999, 1, 1))
+  religion = religions.sample
+  occupation = Faker::Company.profession
+  self_summary = Faker::Hipster.paragraph
+  
+  preferred_gender = preferred_genders.sample
+  looking_for = looking_fors.sample
+  
+  age = Date.today.year - date_of_birth.year
+  min_age = Faker::Number.between(age, age+1)
+  max_age = Faker::Number.between(min_age, 100)
+  
+  lat = Faker::Address.latitude
+  long = Faker::Address.longitude
+  
+  email = Faker::Internet.email
+  password = "foobar1"
+  
+  User.create!(first_name: first_name,
+              last_name: last_name,
+              gender: gender,
+              email: email,
+              date_of_birth: date_of_birth,
+              occupation: occupation,
+              religion: religion,
+              lat: lat,
+              long: long,
+              self_summary: self_summary,
+              looking_for: looking_for,
+              preferred_gender: preferred_gender,
+              min_age: min_age,
+              max_age: max_age,
+              password: password,
+              password_confirmation: password)
+  
+end
