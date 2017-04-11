97efbcf6e5ab4d30454430b7b553d39d839b5a5e
Jasmine Ellis <ellis.jas48@gmail.com>
2017-04-11 05:23:50 +0000

    Add test for minimum age

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -5,10 +5,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, params: { user: { first_name:  "",
-                                         email: "user@invalid",
-                                         password:              "foo",
-                                         password_confirmation: "bar" } }
+      post users_path, params: { user: { first_name: "", last_name: "Klein", email: "natalieklien@example", gender: "female", occupation: "student", religion: "agnostic", city: "", post_code: "3000", country: "Australia", self_summary: "Blah blah whatever whatever", preferred_gender: "male", min_age: 20, max_age: 28, looking_for: "", date_of_birth: Date.new(1994,5,7), password: "foo", password_confirmation: "foobar1" } }
     end
     assert_template 'users/new'
   end
@@ -21,6 +18,15 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_template 'users/show'
     assert is_logged_in?
+  end
+  
+  test "user must be at least 18" do
+    now = Time.now.utc.to_date
+    too_young = Date.new(now.year-18,now.month,now.day+1)
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { first_name: "Natalie", last_name: "Klein", email: "natalieklien@example.com", gender: "female", occupation: "student", religion: "agnostic", city: "Melbourne", post_code: "3000", country: "Australia", self_summary: "Blah blah whatever whatever", preferred_gender: "male", min_age: 20, max_age: 28, looking_for: "long-term dating", date_of_birth: too_young, password: "foobar1", password_confirmation: "foobar1" } }
+    end
+    assert_template 'users/new'
   end
 
 end
