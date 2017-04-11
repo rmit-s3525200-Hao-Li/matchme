5a2ab60ba358f7c402ba083faeffcbea6db224c6
Jasmine Ellis <ellis.jas48@gmail.com>
2017-04-11 12:23:43 +0000

    Assets fix?

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,6 @@ module MatchMe
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.assets.paths << Rails.root.join("app", "assets", "font")
+    config.serve_static_assets = true
   end
 end
