3b23db327041e2729f2cda2813130dd34d614800
Jasmine Ellis <ellis.jas48@gmail.com>
2017-04-11 12:17:47 +0000

    Hide admin profile and configure for production

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Geocoder::Railtie.insert
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module SampleApp
+module MatchMe
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
