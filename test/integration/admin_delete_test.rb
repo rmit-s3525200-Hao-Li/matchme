8c0608fa0bc1c5b5ab8eae4d348feac89590e267
XIEYANG WU <s3499835@student.rmit.edu.au>
2017-04-25 12:52:21 +0000

    admin delete_test

--- test/integration/admin_delete_test.rb
+++ test/integration/admin_delete_test.rb
@@ -2,10 +2,25 @@ require 'test_helper'
 
 class AdminDeleteTest < ActionDispatch::IntegrationTest
   def setup
-  
+    @admin       = users(:admin)
     @non_admin = users(:matt)
   end
-
+ test " admin delete links" do
+    log_in_as(@admin)
+    get users_path
+    assert_template 'users/index'
+    assert_select 'div.pagination'
+    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users.each do |user|
+      assert_select 'a[href=?]', user_path(user), text: user.name
+      unless user == @admin
+        assert_select 'a[href=?]', user_path(user), text: 'delete'
+      end
+    end
+    assert_difference 'User.count', -1 do
+      delete user_path(@non_admin)
+    end
+  end
 
   test "index as non-admin" do
     log_in_as(@non_admin)
@@ -13,3 +28,6 @@ class AdminDeleteTest < ActionDispatch::IntegrationTest
     assert_select 'a', text: 'delete', count: 0
   end
 end
+
+
+  
