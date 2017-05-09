2d2371c5a18f605fdc315e4978af250c42d4c646
rmit-s3525200-Hao-Li <s3525200@student.rmit.edu.au>
2017-05-09 23:28:26 +0000

    Migrate the database for like/unlike function

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,17 @@ class User < ApplicationRecord
   attr_accessor :remember_token
   before_save :downcase_email
   
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+                                  
+  has_many :following, through: :active_relationships, source: :followed 
+  
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+                                   
+   has_many :followers, through: :passive_relationships, source: :follower                                 
   # Validates email
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
@@ -60,6 +71,17 @@ class User < ApplicationRecord
     User.find(match_users)
   end
   
+  def follow(user)
+    active_relationships.create(followed_id: user.id)
+  end
+  
+  def unfollow(user)
+    active_relationships.find_by(followed_id: user.id).destroy
+  end
+  
+  def following?(user)
+    following.include?(user)
+  end
   private
   
     # Converts email to all lower-case
