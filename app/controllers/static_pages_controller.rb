3f023f7a75bdc8f035867332ac012a87787adb31
Jasmine Ellis <ellis.jas48@gmail.com>
2017-05-13 10:40:02 +0000

    Redirect homepage to user matches page if logged in

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,16 @@
 class StaticPagesController < ApplicationController
+  
+  before_action :redirect_logged_in_user
+  
   def home
     @user = User.new
   end
+  
+  private
+  
+    def redirect_logged_in_user
+      if logged_in?
+        redirect_to matches_user_path(current_user)
+      end
+    end
 end
