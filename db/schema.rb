de9e3bfe4a15b6061a824a38078760a54c1e3479
rmit-s3525200-Hao-Li <s3525200@student.rmit.edu.au>
2017-05-15 05:47:16 +0000

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20170509081815) do
-=======
 ActiveRecord::Schema.define(version: 20170514112439) do
->>>>>>> 72d08b1d1c1cd7ce024d15bc40ef940206ab27eb
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,16 +64,6 @@ ActiveRecord::Schema.define(version: 20170514112439) do
     t.string   "music"
     t.string   "name"
     t.index ["user_id"], name: "index_profiles_on_user_id", using: :btree
-  end
-
-  create_table "relationships", force: :cascade do |t|
-    t.integer  "follower_id"
-    t.integer  "followed_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.index ["followed_id"], name: "index_relationships_on_followed_id", using: :btree
-    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true, using: :btree
-    t.index ["follower_id"], name: "index_relationships_on_follower_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
