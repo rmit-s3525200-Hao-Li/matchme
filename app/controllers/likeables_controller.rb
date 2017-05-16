b96961a99767505e8d6509675a5cb27065cb70bc
Jasmine Ellis <ellis.jas48@gmail.com>
2017-05-16 10:34:08 +0000

    Ensure user cannot like themselves

--- app/controllers/likeables_controller.rb
+++ app/controllers/likeables_controller.rb
@@ -1,5 +1,6 @@
 class LikeablesController < ApplicationController
   before_action :logged_in_user
+  before_action :is_other_user, only: :create
 
   def create
     user = User.find(params[:liked_id])
@@ -12,5 +13,16 @@ class LikeablesController < ApplicationController
     current_user.unlike(user)
     redirect_to user
   end
+  
+  private
+    
+    # Ensures users cannot like themselves
+    def is_other_user
+      user = User.find(params[:liked_id])
+      if current_user?(user)
+        flash[:danger] = "You cannot like yourself"
+        redirect_to(user)
+      end
+    end
   
 end
