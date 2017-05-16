a3c64b30278ade61d2982f1f816f8099f93ba101
rmit-s3525200-Hao-Li <s3525200@student.rmit.edu.au>
2017-05-16 12:33:37 +0000

    Change the route from 'login' to 'sign in'

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   get    'static_pages/home'
   get    '/signup',         to: 'users#new'
   post   '/signup',         to: 'users#create'
-  get    '/login',          to: 'sessions#new'
-  post   '/login',          to: 'sessions#create'
+  get    '/signin',          to: 'sessions#new'
+  post   '/signin',          to: 'sessions#create'
   delete '/logout',         to: 'sessions#destroy'
   
    resources :users do
