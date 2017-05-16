7ee69f4146536fc24f48f750b7a8258b65d8d768
Jasmine Ellis <ellis.jas48@gmail.com>
2017-05-16 13:51:02 +0000

    Change login path name

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -7,9 +7,9 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   end
 
   test "login with invalid information" do
-    get login_path
+    get signin_path
     assert_template 'sessions/new'
-    post login_path, params: { session: { email: "", password: "" } }
+    post signin_path, params: { session: { email: "", password: "" } }
     assert_template 'sessions/new'
     assert_not flash.empty?
     get root_path
@@ -18,13 +18,13 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
   test "login with valid information followed by logout" do
     # Check that user can login
-    get login_path
-    post login_path, params: { session: { email: @user.email, password: 'foobar1' } }
+    get signin_path
+    post signin_path, params: { session: { email: @user.email, password: 'foobar1' } }
     assert is_logged_in?
     assert_redirected_to @user
     follow_redirect!
     assert_template 'users/show'
-    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", signin_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
     # Check that user can logout
