c2bde804c56e469ad245f8dbd6d94a00e87e0e7a
XIEYANG WU <s3499835@student.rmit.edu.au>
2017-05-19 07:16:01 +0000

    match valid testing

--- test/models/match_test.rb
+++ test/models/match_test.rb
@@ -2,5 +2,12 @@ require 'test_helper'
 
 class MatchTest < ActiveSupport::TestCase
   
+  def setup
+    @match = Match.new(user_one_id: users(:matt).id, user_two_id: users(:jess).id)
+  end
+  
+  test "should be valid" do
+    assert @match.valid?
+  end
 
 end
