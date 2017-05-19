972452c6e4dbef579fac86fba41934007b989266
XIEYANG WU <s3499835@student.rmit.edu.au>
2017-05-19 07:15:08 +0000

    fix bug

--- test/models/match_test.rb
+++ test/models/match_test.rb
@@ -2,22 +2,5 @@ require 'test_helper'
 
 class MatchTest < ActiveSupport::TestCase
   
-  def setup
-    @match = Match.new(user_one_id: users(:matt).id, user_two_id: users(:jess).id)
-  end
-  
-  test "should be valid" do
-    assert @match.valid?
-  end
-  
-  test "user_one_id should be present" do
-    @match.user_one_id = nil
-    assert_not @match.valid?
-  end
-  
-  test "user_two_id should be present" do
-    @match.user_two_id = nil
-    assert_not @match.valid?
-  end
-  
+
 end
