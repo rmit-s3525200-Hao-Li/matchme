1e5373ed6469bda92fdf7738f989997dfaf00766
XIEYANG WU <s3499835@student.rmit.edu.au>
2017-05-19 07:12:27 +0000

    likeable valid testing in models

--- test/models/likeable_test.rb
+++ test/models/likeable_test.rb
@@ -1,6 +1,14 @@
 require 'test_helper'
 
 class LikeableTest < ActiveSupport::TestCase
-
+  
+  def setup
+    @likeable = Likeable.new(liker_id: users(:matt).id, liked_id: users(:jess).id)
+  end
+  
+  test "should be valid" do
+    assert @likeable.valid?
+  end
+  
 end
   
