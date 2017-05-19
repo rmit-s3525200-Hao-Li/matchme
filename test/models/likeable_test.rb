235136d6a4add956a002cab8a1aaae12d501906b
XIEYANG WU <s3499835@student.rmit.edu.au>
2017-05-19 07:11:07 +0000

    fix bug

--- test/models/likeable_test.rb
+++ test/models/likeable_test.rb
@@ -1,21 +1,6 @@
 require 'test_helper'
 
 class LikeableTest < ActiveSupport::TestCase
-  def setup
-    @likeable = Likeable.new(liker_id: users(:matt).id, liked_id: users(:jess).id)
-  end
-  
-  test "should be valid" do
-    assert @likeable.valid?
-  end
-  
-  test "liker_id should be present" do
-    @likeable.liker_id = nil
-    assert_not @likeable.valid?
-  end
-  test "liked_id should be present" do
-    @likeable.liked_id = nil
-    assert_not @likeable.valid?
-  end
+
 end
   
