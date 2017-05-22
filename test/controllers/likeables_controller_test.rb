e411f4214c1476128fdc9c3858c9157eaae773c3
Jasmine Ellis <ellis.jas48@gmail.com>
2017-05-22 12:51:55 +0000

    Add likeables controller tests

--- test/controllers/likeables_controller_test.rb
+++ test/controllers/likeables_controller_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
-class RelationshipsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+class LikeablesControllerTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @user = users(:matt)
+  end
+  
+  test "create should require logged-in user" do
+    assert_no_difference 'Likeable.count' do
+      post likeables_path
+    end
+    assert_redirected_to signin_url
+  end
+
+  test "destroy should require logged-in user" do
+    assert_no_difference 'Likeable.count' do
+      delete likeable_path(likeables(:one))
+    end
+    assert_redirected_to signin_url
+  end
+  
 end
