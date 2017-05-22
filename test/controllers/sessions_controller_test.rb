db1f2ee0dba4162c5077efd334eab99127719ecb
Jasmine Ellis <ellis.jas48@gmail.com>
2017-05-22 13:27:53 +0000

    Add test for sessions controller

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -2,9 +2,26 @@ require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
   
+  def setup
+    @user = users(:matt)
+    @other_user = users(:jess)
+  end
+  
   test "should get new" do
     get signin_path
     assert_response :success
+  end
+  
+  test "should not get new if logged in" do
+    log_in_as(@user)
+    get signin_path
+    assert_redirected_to root_url
+  end
+  
+  test "should not create if logged in" do
+    log_in_as(@user)
+    post signin_path, params: { session: { email: @other_user.email, password: 'foobar1', remember_me: '1' } }
+    assert_redirected_to root_url
   end
 
 end
