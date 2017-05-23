dd4f755a373942cbacc82f5828108adad489923a
Jasmine Ellis <ellis.jas48@gmail.com>
2017-05-23 11:41:27 +0000

    Add default scope to likeable so that they're ordered by created_at

--- app/models/likeable.rb
+++ app/models/likeable.rb
@@ -1,6 +1,11 @@
 class Likeable < ApplicationRecord
+    
     belongs_to :liker, class_name: "User"  
-    belongs_to :liked, class_name: "User"  
+    belongs_to :liked, class_name: "User"
+    
+    default_scope -> { order('created_at DESC') }
+    
     validates :liker_id, presence: true  
-    validates :liked_id, presence: true  
+    validates :liked_id, presence: true
+    
 end
