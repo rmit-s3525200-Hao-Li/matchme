5e4300d54030bf944ba028cbac4f86900de78f96
XIEYANG WU <s3499835@student.rmit.edu.au>
2017-05-25 02:55:55 +0000

    should not get new if logged in in testing

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,10 +1,14 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  
-  test "should get new" do
-    get signup_path
-    assert_response :success
+  def setup
+    @user       = users(:matt)
+    @other_user = users(:xieyang)
   end
-
+  
+  test "should not get new if logged in" do
+    log_in_as(@user)
+    get users_path
+    assert_redirected_to root_url
+  end
 end
